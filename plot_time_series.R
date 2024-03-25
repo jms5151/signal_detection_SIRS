f5f111a38a27ca677f0f1e29b42c3005967bb217
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2024-03-25 15:30:21 -0400

    update time series plots for new simulations

--- plot_time_series.R
+++ plot_time_series.R
@@ -1,3 +1,40 @@
+## plot simulations
+
+# load data
+clim_data_path <- '../data/sim_climate/'
+sim_data_path <- '../data/sim_results/'
+
+clim_files <- list.files(clim_data_path, full.names = T)
+dis_files <- list.files(sim_data_path, full.names = T)
+
+for(j in 1:length(clim_files)){
+  c <- readRDS(clim_files[j])
+  d <- readRDS(dis_files[j])
+  lineCols <- rainbow(n = length(c))
+  titleName <- gsub('../data/sim_climate/normal_|_clim.RData', '', clim_files[j])
+  fileName <- paste0('../figures/time_series/sim_', titleName, '.pdf')
+  if(titleName == 'wet' | titleName == 'dry' | titleName == 'moderate'){
+    yrange = c(0, 400)
+  } else {
+    yrange = c(-20, 35)
+  }
+  pdf(fileName, width = 5, height = 6)
+  par(mfrow = c(3,1), mar = c(1,4,2,1))
+  # climate plot
+  plot(c[[1]], type = 'l', main = titleName, ylab = 'Climate', ylim = yrange)
+  for(k in 1:length(d)){lines(c[[k]], col = lineCols[k])}
+  # infected plot
+  plot(d[[1]]$I, type = 'l', ylim = c(0, max(d[[1]]$I)+0.03), ylab = 'Infected', xlab = '')
+  for(l in 1:length(d)){lines(d[[l]]$I, col = lineCols[l])}
+  # susceptilbe plot
+  plot(d[[1]]$S, type = 'l', ylim = c(0, 1), ylab = 'Susceptible', xlab = '')
+  for(m in 1:length(d)){lines(d[[m]]$S, col = lineCols[m])}
+  dev.off()
+}
+
+# old
+
+
 files <- list.files('../results/', full.names = T)
 wbdfiles <- files[grepl('wbd', files)]
 vbdfiles <- files[grepl('vbd', files)]
@@ -24,5 +61,3 @@ for(i in vbdfiles){
 }
 dev.off()
 
-
-x2 <- x[grepl('normal', x)]

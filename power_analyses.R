5e588f627cc0c00159af0b12c95217cfa1bbf241
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2024-04-15 15:06:57 -0400

    update power analyses with new summary data

--- power_analyses.R
+++ power_analyses.R
@@ -1,15 +1,11 @@
-source('filepaths.R')
-
-summaryFiles <- list.files('../data/results/', full.names = T)
-summaryFiles <- summaryFiles[grepl('short', summaryFiles)]
-
-# Load the tidyr package
-library(tidyverse)
-# library(tidyr)
-library(stringr)
+# load libraries
+library(tidyr)
 library(pwrss)
 
+# read in file
+x <- readRDS('../data/long_summary.RData')
 
+# function to calculate power
 calc_power <- function(mean1, mean2, sd1, sd2, n1, n2){
   p <- pwrss.t.2means(mu1 = mean1
                       , mu2 = mean2
@@ -22,74 +18,31 @@ calc_power <- function(mean1, mean2, sd1, sd2, n1, n2){
   return(p$power)  
 }
 
+# reorganize data
+reorganized_data <- x %>%
+  pivot_longer(cols = c(final_size, mean_beta, beta_sd, Re_kurtosis, max_Re, max_Re_timing),
+               names_to = "metric",
+               values_to = "value") %>%
+  group_by(metric, experiment, filename, ee) %>%
+  summarize(
+    mean_value = mean(value),
+    sd_value = sd(value),
+    n = length(value)
+  ) %>%
+  pivot_wider(names_from = experiment,
+              values_from = c(mean_value, sd_value, n),
+              names_sep = "_") %>%
+  as.data.frame()
 
-countries <- c('Brazil', 'BurkinaFaso', 'Fiji', 'Italy', 'Pakistan', 'Philippines', 'China', 'Ethiopia', 'Haiti', 'India', 'Sudan')
+# calculate power
+reorganized_data$power <- mapply(calc_power, 
+                                 reorganized_data$mean_value_control
+                                 , reorganized_data$mean_value_experiment
+                                 , reorganized_data$sd_value_control
+                                 , reorganized_data$sd_value_experiment
+                                 , reorganized_data$n_control
+                                 , reorganized_data$n_experiment)
 
-summary_df <- c()
-
-# combine summary data
-for(i in summaryFiles){
-  # read file
-  results_flattened <- readRDS(i)
-  
-  # summarize return time and correlation variables
-  results_flattened$Return_time[results_flattened$Return_time == 99999] <- NA
-  
-  returnAndCorr <- results_flattened %>%
-    group_by(Dataset) %>%
-    summarise(Return_time = mean(Return_time, na.rm = T)
-              , CE_correlation = mean(CE_Correlation)) %>%
-    pivot_longer(cols = c(Return_time, CE_correlation)
-                 , names_to = "Variable"
-                 , values_to = "Value")
-  
-  # format data for power analyses
-  long_dataset <- results_flattened %>%
-    pivot_longer(
-      cols = -c(Dataset), # Keeps 'Dataset' column as is
-      names_to = c(".value", "Variable"), # Directs how to transform and name parts of the column names
-      names_pattern = "(Control|Experiment)_(.+)", # Regular expression to match and split the column names
-      values_to = c("Control", "Experiment") # Names for the reshaped value columns
-    ) %>%
-    na.omit()
-  
-  long_dataset <- long_dataset %>%
-    group_by(Dataset, Variable) %>%
-    summarise(mu_control = mean(Control)
-              , sd_control = sd(Control)
-              , mu_experiment = mean(Experiment)
-              , sd_experiment = sd(Experiment)
-              , n_control = length(Control)
-              , n_experiment = length(Experiment)) 
-  
-  long_dataset$Value <- mapply(calc_power, long_dataset$mu_control, long_dataset$mu_experiment, long_dataset$sd_control, long_dataset$sd_experiment, long_dataset$n_control, long_dataset$n_experiment)
-  
-  # combine dataframes
-  df <- rbind(long_dataset[,c('Dataset', 'Variable', 'Value')], returnAndCorr)
-  df <- as.data.frame(df)
-  
-  # add magnitude, duration, event timing
-  split_columns <- as.data.frame(str_split_fixed(df$Dataset, "_", n = Inf))
-  
-  df$magnitude <- split_columns$V1
-  df$magnitude <- gsub('I', '', df$magnitude)
-  df$magnitude <- as.numeric(df$magnitude)
-  
-  df$duration <- split_columns$V2
-  df$duration <- gsub('D', '', df$duration)
-  df$duration <- as.numeric(df$duration)
-  
-  df$ee_timing <- split_columns$V3
-  
-  # add identifiers
-  df$diseaseSystem <- ifelse(grepl('vbd', i), 'vbd', 'wbd')
-  df$timeWindow <- ifelse(grepl('short', i), 'short', 'long')
-  df$region <- countries[str_detect(i, countries)]
-  
-  # row bind data
-  summary_df <- rbind(summary_df, df)
-}
-
-saveRDS(summary_df, file = '../data/ee_summary_all.RData')
-
+# save
+saveRDS(reorganized_data, file = '../data/power_summary.RData')
 

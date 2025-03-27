452d242b3a96aa770614ff6bd283278948362f15
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2025-03-27 12:22:23 -0400

    update validation analysis and plots

--- analyze_prior_ex_events.R
+++ analyze_prior_ex_events.R
@@ -14,7 +14,9 @@ koppen <- read.csv('../EE_Koppen_filled.csv')
 # join data
 csid2 <- csid %>%
   left_join(ae) %>%
-  left_join(koppen)
+  left_join(koppen) %>%
+  mutate(Climate_threshold_80 = NA, Climate_value = NA) %>%
+  filter(!is.na(ID))
 
 # format dates
 csid2[,c('Date_start', 'Date_end', 'Date_peak')] <- lapply(csid2[,c('Date_start', 'Date_end', 'Date_peak')], function(x) as.Date(x, '%m/%d/%Y'))
@@ -25,25 +27,70 @@ clim_files <- list.files(clim_files_path)
 
 # Add percentiles from historical data
 for(i in 1:nrow(csid2)){
-  if(is.na(csid2$Percentile[i])){
-    ee_id <- csid2$ID[i]
-    fileID <- which(grepl(paste0('ID', ee_id, '_'), clim_files))
-    clim_data <- read.csv(paste0(clim_files_path, clim_files[fileID]))
-    clim_data$Date <- as.Date(clim_data$time)
-    if(!is.na(csid2$Date_peak[i])){
-      ee_value <- clim_data$X0[which(clim_data$Date == csid2$Date_peak[i])]
+  ee_id <- csid2$ID[i]
+  fileID <- which(grepl(paste0('ID', ee_id, '_'), clim_files))
+  clim_data <- read.csv(paste0(clim_files_path, clim_files[fileID]))
+  clim_data$Date <- as.Date(clim_data$time)
+  if(csid2$Climate_variable[i] == 'Rainfall'){
+    clim_data$YM <- format(clim_data$Date, '%Y-%m')
+    clim_summary <- clim_data %>%
+      group_by(YM) %>%
+      summarise(X0sum = sum(X0, na.rm = T)) %>%
+      as.data.frame()
+  csid2$Climate_threshold_80[i] <- quantile(clim_summary$X0sum, 0.8)
+  if(!is.na(csid2$Date_peak[i])){
+    csid2$Climate_value[i] <- clim_summary$X0sum[which(clim_summary$YM == format(csid2$Date_peak[i], '%Y-%m'))]
+    } else if(!is.na(csid2$Date_start[i]) & !is.na(csid2$Date_end[i])){
+      csid2$Climate_value[i] <- max(clim_summary$X0sum[which(clim_summary$YM == format(csid2$Date_start[i], '%Y-%m')):which(clim_summary$YM == format(csid2$Date_end[i], '%Y-%m'))])
     } else {
-      ee_value <- max(clim_data$X0[which(clim_data$Date == csid2$Date_start[i]):which(clim_data$Date == csid2$Date_end[i])])
+      csid2$Climate_value[i] <- NA
+    }  
+  if(is.na(csid2$Percentile[i])){
+    clim_distr <- ecdf(clim_summary$X0sum)
+    csid2$Percentile[i] <- round(clim_distr(csid2$Climate_value[i]) * 100)
     }
-    clim_distr <- ecdf(clim_data$X0)
-    csid2$Percentile[i] <- round(clim_distr(ee_value) * 100)
+  }
+  else { # if temperature data
+    if(!is.na(csid2$Date_peak[i])){
+      csid2$Climate_value[i] <- clim_data$X0[which(clim_data$Date == csid2$Date_peak[i])]
+    } else if(!is.na(csid2$Date_start[i]) & !is.na(csid2$Date_end[i])){
+      csid2$Climate_value[i] <- max(clim_data$X0[which(clim_data$Date == csid2$Date_start[i]):which(clim_data$Date == csid2$Date_end[i])])
+    } else {
+      csid2$Climate_value[i] <- NA
+    }
+    csid2$Climate_threshold_80[i] <- quantile(clim_data$X0, 0.8)
+    if(is.na(csid2$Percentile[i])){
+      clim_distr <- ecdf(clim_data$X0)
+      csid2$Percentile[i] <- round(clim_distr(csid2$Climate_value[i]) * 100)
+    }
   }
 }
-
+  
 # average percentiles across studies
-x <- csid2 %>%
-  group_by(ID, City, lon, lat, Disease, Climate_variable, Outbreak_risk, Extreme_climate_event, Koppen_Name, Agreement, Evidence) %>%
-  summarise(Percentile = median(Percentile))
+# x <- csid2 %>%
+#   group_by(ID, City, lon, lat, Disease, Climate_variable, Outbreak_risk, Extreme_climate_event, Koppen_Name, Agreement, Evidence) %>%
+#   summarise(Percentile = median(Percentile)
+#             , Clim = median(Climate_median)
+#             ) %>%
+#   as.data.frame()
+# 
+# # round and average by clim and then plot?
+# xx <- subset(x, Climate_variable == 'Rainfall' & Disease == 'Cholera')
+# plot(xx$Clim, xx$Percentile, pch = 16, col = as.factor(xx$Outbreak_risk))
+# 
+# xx <- subset(csid2, Climate_variable == 'Temperature')
+# plot(xx$Climate_median, xx$Percentile, pch = 16, col = as.factor(xx$Disease))
+# 
+# x1 <- x %>%
+#   mutate(Climate2 = round(Clim, 1)) %>%
+#   filter(Climate_variable == 'Rainfall') %>%
+#   group_by(Climate2) %>%
+#   mutate(mid = quantile(Percentile, 0.5)) %>% as.data.frame()
+#             , lwr = quantile(Percentile, 0.25)
+#             , upr = quantile(Percentile), 0.75)
+# 
+# plot(x1$Climate2, x1$mid, col = as.factor(xx$Outbreak_risk))
+x <- csid2
 
 # format text for plotting
 x$Outbreak_risk <- gsub('No.*', 'No', x$Outbreak_risk)
@@ -75,6 +122,8 @@ x$Koppen_Subgroup <- gsub('Dfb', 'No dry season,\n Warm summer', x$Koppen_Subgro
 x$Extreme_climate_event[x$Extreme_climate_event == 'Cyclone/Hurricane/Typhoon'] <- 'Cyclone/\nHurricane/Typhoon'
 
 x$Agreement[x$Agreement == ""] <- NA
+
+# x$Climate_threshold_80[x$Climate_threshold_80 == 1] <- 0.330750
 
 # save
 write.csv(x, file = '../data/extreme_events.csv', row.names = F)
--- plot_historical_ee_driven_outbreaks.R
+++ plot_historical_ee_driven_outbreaks.R
@@ -3,6 +3,8 @@
 # load libraries
 library(ggplot2)
 library(patchwork)
+library(dplyr)
+library(purrr)
 
 # source('analyze_prior_ex_events.R) needed to be run first
 
@@ -11,6 +13,7 @@ x <- read.csv('../data/extreme_events.csv')
 x$Percentile[x$Extreme_climate_event == 'Drought'] <- 100 - x$Percentile[x$Extreme_climate_event == 'Drought']
 x$Extreme_climate_event[x$Extreme_climate_event == 'Cyclone/Hurricane/Typhoon'] <- 'Cyclone/\nHurricane/Typhoon'
 
+# Plotting extreme events: intensity vs group categories
 # Plotting function
 plotFun <- function(xName, colorName, custom_colors, colorLegendName, titleName){
   p <- ggplot(x, aes(x = xName, y = Percentile, color = colorName)) +
@@ -52,3 +55,144 @@ combined_plot
 # save
 ggsave(filename = '../figures/csid_comparison.pdf', plot = combined_plot, width = 12, height = 6.5)
 
+# Attributable fraction for cholera
+source('far_and_rr_functions.R')
+
+# Define functions
+# Beta function
+betafun <- function(b1, bW, rainfall) {
+  b1 + (bW * rainfall)
+}
+
+# AF curve for one (b1, bW)
+calc_AF_curve <- function(p0_rain, rain, b1, bW) {
+  p0 <- betafun(b1, bW, p0_rain)
+  p1s <- betafun(b1, bW, rain)
+  FAR <- calc_FAR(p0, p1s)
+  RR <- calc_RR(p0, p1s)
+  calc_AF(FAR, RR)
+}
+
+# Deviation from threshold
+calc_raindev <- function(rain, threshold) {
+  rain / threshold
+}
+
+# Theory curves with uncertainty ribbons
+make_theory_df_with_uncertainty <- function(quants, rain, bvals) {
+  labels <- c("Dry", "Moderate", "Wet")
+  
+  purrr::map2_dfr(
+    .x = quants,
+    .y = labels,
+    .f = function(q, label) {
+      raindev <- calc_raindev(rain, q)
+      
+      af_mat <- purrr::map2_dfc(
+        .x = bvals$b1,
+        .y = bvals$bW,
+        .f = function(b1, bW) {
+          calc_AF_curve(q, rain, b1, bW)
+        }
+      )
+      
+      data.frame(
+        raindev = raindev,
+        AF_mean = rowMeans(af_mat),
+        AF_lower = apply(af_mat, 1, quantile, probs = 0.025),
+        AF_upper = apply(af_mat, 1, quantile, probs = 0.975),
+        threshold = label
+      )
+    }
+  )
+}
+
+# Observed AF with uncertainty per row
+calc_observed_AF_uncertainty <- function(df, bvals) {
+  af_list <- purrr::pmap(
+    .l = list(b1 = bvals$b1, bW = bvals$bW),
+    .f = function(b1, bW) {
+      p0 <- betafun(b1, bW, df$Climate_threshold_80)
+      p1 <- betafun(b1, bW, df$Climate_value)
+      FAR <- calc_FAR(p0, p1)
+      RR <- calc_RR(p0, p1)
+      calc_AF(FAR, RR)
+    }
+  )
+  
+  af_mat <- do.call(cbind, af_list)
+  
+  df$AF_mean <- rowMeans(af_mat)
+  df$AF_lower <- apply(af_mat, 1, quantile, probs = 0.025)
+  df$AF_upper <- apply(af_mat, 1, quantile, probs = 0.975)
+  
+  return(df)
+}
+
+# Input data
+bvals <- data.frame(
+  b1 = c(0.212, 0.243, 0.155, 0.214),
+  bW = c(0.00432, 0.00128, 0.00292, 0.00108)
+)
+
+# Subset cholera rows with non-missing rainfall
+xrain <- subset(x, !is.na(Climate_value) & Disease == 'Cholera')
+
+# Rain sequence
+maxRain <- round(max(xrain$Climate_value))
+rain <- seq(0, maxRain, 0.5)
+
+# Rainfall threshold quantiles
+xrain_quants <- quantile(xrain$Climate_threshold_80, c(0.1, 0.5, 0.9))
+
+# Create theory data with uncertainty
+af_theory_df <- make_theory_df_with_uncertainty(unname(xrain_quants), rain, bvals)
+
+# Apply observational uncertainty
+xrain <- xrain %>%
+  mutate(
+    raindev = calc_raindev(Climate_value, Climate_threshold_80),
+    climate_regime = case_when(
+      Climate_threshold_80 < quantile(Climate_threshold_80, 0.2) ~ 'Dry',
+      Climate_threshold_80 > quantile(Climate_threshold_80, 0.8) ~ 'Wet',
+      TRUE ~ 'Moderate'
+    )
+  )
+xrain <- calc_observed_AF_uncertainty(xrain, bvals)
+
+# Colors
+threshold_cols <- c('Dry' = 'brown', 'Moderate' = 'lightblue', 'Wet' = 'blue')
+
+# Plot
+# ribbon in supplemental
+af_obs_plot <- ggplot() +
+  # Theoretical uncertainty ribbon
+  geom_ribbon(data = af_theory_df,
+              aes(x = raindev, ymin = AF_lower, ymax = AF_upper, fill = threshold),
+              alpha = 0.3) +
+  # Theoretical mean line
+  geom_line(data = af_theory_df,
+            aes(x = raindev, y = AF_mean, color = threshold),
+            size = 1) +
+  # Observational error bars
+  geom_errorbar(data = xrain,
+                aes(x = raindev, ymin = AF_lower, ymax = AF_upper, color = climate_regime),
+                width = 0.1) +
+  # Observational points
+  geom_point(data = xrain,
+             aes(x = raindev, y = AF_mean, fill = climate_regime),
+             shape = 21, size = 3, stroke = 0.3, color = 'black') +
+  # Colors
+  scale_color_manual(values = threshold_cols) +
+  scale_fill_manual(values = threshold_cols) +
+  labs(x = 'Relative Ratio (total rainfall / exceedance threshold)',
+       y = 'Attributable Fraction',
+       fill = 'Climate regime',
+       color = 'Climate regime') +
+  theme_minimal(base_size = 13) +
+  theme(legend.position = 'top') +
+  theme_bw() +
+  xlim(1,6) 
+
+ggsave(filename = '../figures/af_obs_plot.pdf', plot = af_obs_plot, width = 8, height = 4)
+ggsave(filename = '../figures/af_obs_plot_line_uncert.pdf', plot = af_obs_plot, width = 8, height = 4)
